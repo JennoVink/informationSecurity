74ff94173b52ccf165a68c0e53624d1ed369d905
BoersmaJ <boersmajaj@gmail.com>
2018-09-11 16:23:54 +0200

    Math lib fails, working on std. dev

--- exercise4.py
+++ exercise4.py
@@ -4,6 +4,7 @@ import sys
 import argparse as ap
 from collections import deque
 from wordsegment import load, segment
+import math
 
 key = 'integrity'
 
@@ -38,8 +39,50 @@ def downloadFileFromICCE():
 file = downloadFileFromICCE()
 
 decrypted = decrypt(file, key)
-print decrypted
-print '---------------------'
-load()
-segment = segment(decrypted)
-print " ".join(str(x) for x in segment)
+# Outputs an freqency table depending on the keyLength
+def getFrequencyTable(text, keylength):
+    i = 0
+    frequencyTable = initFrequencyTable(keylength)
+    for c in text.replace("\n", ""):
+        frequencyTable[i][c] += 1
+        i += 1
+        i %= keylength
+
+    return frequencyTable
+
+def initFrequencyTable(keylength):
+    freqTable = {}
+    for i in range (0, keylength):
+        freqTable[i] = {}
+
+        for c in string.ascii_lowercase:
+            freqTable[i][c] = 0
+
+    return freqTable
+
+def getStandardDeviation(sumOfSquared, sum):
+    tst = (sum / 26) * (sum / 26)
+    print tst
+    return math.sqrt(sumOfSquared / 26 - tst)
+
+for i in range(5, 16):
+    sumOfSquared = 0
+    sum = 0
+    standardDevSum = 0
+    frequencyTable = getFrequencyTable(file, i)
+    #print frequencyTable
+    for letterIndex in frequencyTable:
+        for frequency in frequencyTable[letterIndex].values():
+            sumOfSquared += frequency * frequency
+            sum += frequency
+
+        standardDevSum += getStandardDeviation(sumOfSquared, sum)
+
+    print 'sum of \t' + str(i) + ' std. devs: ' + str(standardDevSum) + ','
+
+#decrypted = decrypt(file, key)
+#print decrypted
+#print '---------------------'
+#load()
+#segment = segment(decrypted)
+# print " ".join(str(x) for x in segment)
