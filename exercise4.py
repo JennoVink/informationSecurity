724b851f78a5c5feb361e7a5f0867e7cb46def19
Jenno Vink <jenno-vink@hotmail.com>
2018-09-11 22:58:51 +0200

    The math failed because of a wrong computation. That is now all fixed.
    Got also the decryption ready. It now can calc the std dev. and the keylength. Then got the key with the frequency table.

--- exercise4.py
+++ exercise4.py
@@ -5,17 +5,18 @@ import argparse as ap
 from collections import deque
 from wordsegment import load, segment
 import math
+import operator
 
-key = 'integrity'
 
-keyInt = []
-for i in key:
-    keyInt.append(string.ascii_lowercase.index(i))
-
-print keyInt
+def keyToIntShift(key):
+    keyInt = []
+    for i in key:
+        keyInt.append(string.ascii_lowercase.index(i))
+    return keyInt
 
 def decrypt(text, key):
     index = 0
+    keyInt = keyToIntShift(key)
     decryptedText = ''
     for c in text.replace("\n", ""):
         charToIndex = string.ascii_lowercase.index(c)
@@ -36,9 +37,7 @@ def downloadFileFromICCE():
     urllib2.install_opener(opener)
     return urllib2.urlopen(url).read()
 
-file = downloadFileFromICCE()
 
-decrypted = decrypt(file, key)
 # Outputs an freqency table depending on the keyLength
 def getFrequencyTable(text, keylength):
     i = 0
@@ -60,29 +59,54 @@ def initFrequencyTable(keylength):
 
     return freqTable
 
-def getStandardDeviation(sumOfSquared, sum):
-    tst = (sum / 26) * (sum / 26)
-    print tst
-    return math.sqrt(sumOfSquared / 26 - tst)
+def sqr(x):
+    return x * x
 
+def getStandardDeviation(sumOfSquared, sum):
+    return math.sqrt(sumOfSquared / 26 - sqr(sum / 26))
+
+file = downloadFileFromICCE()
+
+# std devs:
+largestStdDev = 0
+bestKeylength = 0
 for i in range(5, 16):
-    sumOfSquared = 0
-    sum = 0
     standardDevSum = 0
     frequencyTable = getFrequencyTable(file, i)
     #print frequencyTable
     for letterIndex in frequencyTable:
+        sumOfSquared = 0
+        sum = 0
         for frequency in frequencyTable[letterIndex].values():
             sumOfSquared += frequency * frequency
-            sum += frequency
+            sum += frequency          
 
         standardDevSum += getStandardDeviation(sumOfSquared, sum)
 
+    if standardDevSum > largestStdDev:
+        largestStdDev = standardDevSum
+        bestKeylength = i
     print 'sum of \t' + str(i) + ' std. devs: ' + str(standardDevSum) + ','
 
-#decrypted = decrypt(file, key)
-#print decrypted
-#print '---------------------'
-#load()
-#segment = segment(decrypted)
-# print " ".join(str(x) for x in segment)
+print 'So that makes a keylength of ' + str(bestKeylength) + ' the most plausible'
+frequencyTable = getFrequencyTable(file, 9)
+
+key = ''
+for letterIndex in frequencyTable:
+    mostFreqChar = sorted(frequencyTable[letterIndex].items(), key=operator.itemgetter(1), reverse=True)[0][0]
+    index = string.ascii_lowercase.index(mostFreqChar)
+    indexOfE = string.ascii_lowercase.index('e')
+    shifting = index - indexOfE
+    # To get the key:
+    key += string.ascii_lowercase[shifting]
+
+    print mostFreqChar + ' is the most frequent char at pos ' + str(letterIndex) + ', (shifting = ' + str(shifting) + ') ==> keychar = \t' + string.ascii_lowercase[shifting]
+
+print 'Found the key! It must be "' + str(key) + '". Now decrypting the text:'
+
+decrypted = decrypt(file, key)
+print decrypted
+print '---------------------'
+load()
+segment = segment(decrypted)
+print " ".join(str(x) for x in segment)
