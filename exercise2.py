826bb851938621c502552175bba173d5da49fe56
Jenno Vink <jenno-vink@hotmail.com>
2018-09-12 11:48:32 +0200

    Finished exercise 2

--- exercise2.py
+++ exercise2.py
@@ -1,11 +1,9 @@
 import urllib2
 import string
 import sys
-import argparse as ap
 from collections import deque
 
-# Mapping as provied by the lecturer.
-mapping = 'zyxwvtsrqpnmlkjhgfdcbuoiea'
+mapping = ''
 
 nonLettersAsIs = False
 decryptArg = -1
@@ -28,11 +26,9 @@ if '-i' in str(sys.argv):
 # e.g. input 1 gives bcdefghijklmnopqrstuvwxyza
 ###
 def shiftToMapping(arg):
-    shift = 0
-    print (arg, type(arg))
     try:
         shift = int(arg)
-        if(shift >= 1 and shift < 26):
+        if shift >= 1 and shift < 26:
             mapping = deque(list(string.ascii_lowercase))
             mapping.rotate(shift * decryptArg)
             return ''.join(mapping)
@@ -46,6 +42,7 @@ def shiftToMapping(arg):
             print 'The mapping does not contain 26 characters.'
             return shiftToMapping(raw_input("Enter new shift value or mapping (26 chars): "))
 
+
 mapping = shiftToMapping(sys.argv[-1])
 
 print 'mapping: '
@@ -58,6 +55,7 @@ for i in range(len(mapping)):
     else:
         letters[list(string.ascii_lowercase)[i]] = mapping[i]
 
+
 def decrypt(text, letters):
     global nonLetterAsIs
     decryptedText = ''
@@ -68,21 +66,17 @@ def decrypt(text, letters):
             decryptedText += c
     return decryptedText
 
-def downloadFileFromICCE():
-    username = 'Security'
-    password = 'First!'
-    url = 'https://www.icce.rug.nl/edu/infosec/intro/2018.enc'
-    p = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    p.add_password(None, url, username, password)
 
-    handler = urllib2.HTTPBasicAuthHandler(p)
-    opener = urllib2.build_opener(handler)
-    urllib2.install_opener(opener)
-    return urllib2.urlopen(url).read()
+def getEncryptedText():
+    with open('2018.enc.txt', 'r') as content_file:
+        content = content_file.read()
+    return content
 
-file = downloadFileFromICCE()
+
+file = getEncryptedText()
 if manualInput:
     file = sys.argv[-2]
+print '---------- Original Input: ----------'
 print file
 
 if decryptArg == 1:
